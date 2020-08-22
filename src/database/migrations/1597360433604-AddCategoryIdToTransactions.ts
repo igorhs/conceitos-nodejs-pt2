963a806093f216ed3a39906ecab74b17cc5af247
Igor Henrique <ihsilva2006@gmail.com>
2020-08-22 12:16:57 -0300

    Testing Feature: List

---
+++ src/database/migrations/1597360433604-AddCategoryIdToTransactions.ts
@@ -0,0 +1,38 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableForeignKey,
+  TableColumn,
+} from 'typeorm';
+
+export default class AddCategoryIdToTransactions1597360433604
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'CategoryTransactions',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'CategoryTransactions');
+
+    await queryRunner.dropColumn('transactions', 'category_id');
+  }
+}
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,69 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getRepository(Transaction);
+
+    const categoriesRepository = getRepository(Category);
+
+    const checkCategoryExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (checkCategoryExists) {
+      const transaction = transactionsRepository.create({
+        title,
+        value,
+        type,
+        category_id: checkCategoryExists.id,
+      });
+
+      if (transaction.type !== 'income' && transaction.type !== 'outcome') {
+        throw new AppError('Invalid type of transaction');
+      }
+
+      await transactionsRepository.save(transaction);
+
+      return transaction;
+    }
+
+    const newCategory = categoriesRepository.create({
+      title: category,
+    });
+
+    await categoriesRepository.save(newCategory);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: newCategory.id,
+    });
+
+    if (transaction.type !== 'income' && transaction.type !== 'outcome') {
+      throw new AppError('Invalid type of transaction');
+    }
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
